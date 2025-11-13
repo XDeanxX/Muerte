d830deba96d40b3d5516a3f50c0f762bfc56d88a
XxDeanxX <12345jesuschaviel@gmail.com>
2025-11-12 20:31:26 -0400

    sin md

--- resources/views/components/layouts/rbac.blade.php
+++ resources/views/components/layouts/rbac.blade.php
@@ -10,6 +10,8 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
 </head>
 
